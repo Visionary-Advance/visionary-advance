2a041f8145d97c85c1ccba07310c1cb0edb89920
Brandon <Brandon@visionaryadvance.com>
2026-02-22 21:42:49 -0800

    Chage

--- Components/ReCaptchaScript.jsx
+++ Components/ReCaptchaScript.jsx
@@ -1,14 +1,16 @@
 // Components/ReCaptchaScript.jsx
-// Component to load reCAPTCHA v3 script
+// Component to load reCAPTCHA v3 script (skipped on admin pages)
 
 'use client'
 
 import Script from 'next/script'
+import { usePathname } from 'next/navigation'
 
 export default function ReCaptchaScript() {
   const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_KEY
+  const pathname = usePathname()
 
-  if (!siteKey) {
+  if (!siteKey || pathname?.startsWith('/admin')) {
     return null
   }
 
